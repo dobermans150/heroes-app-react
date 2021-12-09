fbf050bcb14eb3add16323ef4d5e4a787b26582a
Christian <christianjguevara@hotmail.com>
2021-12-09 12:50:20 -0400

    navLink class

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -3,11 +3,16 @@ import { Link, NavLink } from 'react-router-dom'
 
 export const Navbar = () => {
 
+    const navLinkClass = ( { isActive } ) => "nav-item nav-link" + ( isActive ? " active" : '' );
+
+
     const handleLogout = () => {
         console.log( 'por hacer' );
     }
 
     return (
+
+
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
 
             <Link
@@ -21,14 +26,14 @@ export const Navbar = () => {
                 <div className="navbar-nav">
 
                     <NavLink
-                        className={ ( { isActive } ) => "nav-item nav-link" + ( isActive ? " active" : '' ) }
+                        className={ ( isActive ) => navLinkClass( isActive ) }
                         to="/marvel"
                     >
                         Marvel
                     </NavLink>
 
                     <NavLink
-                        className={ ( { isActive } ) => "nav-item nav-link" + ( isActive ? " active" : '' ) }
+                        className={ ( isActive ) => navLinkClass( isActive ) }
                         to="/dc"
                     >
                         DC
